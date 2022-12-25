bfe7ea0a93c20b33fd24b6c3bb7fa840a8eca773
VinhNguyen217 <nguyenducvinhthd@gmail.com>
2022-12-25 21:17:02 +0700

    fix sql

--- yogo.sql
+++ yogo.sql
@@ -23,16 +23,19 @@ DROP TABLE IF EXISTS `booking`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `booking` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` varchar(255) NOT NULL,
   `created_at` datetime(6) DEFAULT NULL,
-  `end_point` double DEFAULT NULL,
   `finish_at` datetime(6) DEFAULT NULL,
-  `service_id` int DEFAULT NULL,
-  `start_point` double DEFAULT NULL,
+  `lat_end_point` double DEFAULT NULL,
+  `lat_start_point` double DEFAULT NULL,
+  `lon_end_point` double DEFAULT NULL,
+  `lon_start_point` double DEFAULT NULL,
+  `service_id` varchar(255) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL,
-  `user_id` int DEFAULT NULL,
+  `total_price` double DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +44,7 @@ CREATE TABLE `booking` (
 
 LOCK TABLES `booking` WRITE;
 /*!40000 ALTER TABLE `booking` DISABLE KEYS */;
-INSERT INTO `booking` VALUES (1,NULL,123.123124124,NULL,1,34.1232141,'CREATED',1),(2,'2022-12-21 23:16:55.977523',123.123124124,NULL,1,34.1232141,'CREATED',1);
+INSERT INTO `booking` VALUES ('872ab47d-e7e9-44e7-8b38-8362159684d3','2022-12-25 21:11:26.531541',NULL,23.414971312,10.213412213213,-23.321873621783,23.23821749214,'54bd1fe4-c478-4056-b5b8-6b37d644d782','CREATED',344000,'c230680a-f169-4058-b118-89c5b4daf496');
 /*!40000 ALTER TABLE `booking` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -53,11 +56,11 @@ DROP TABLE IF EXISTS `bookmark`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `bookmark` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` varchar(255) NOT NULL,
   `created_at` datetime(6) DEFAULT NULL,
   `default_location` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
-  `user_id` int DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -79,12 +82,12 @@ DROP TABLE IF EXISTS `oder`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `oder` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` varchar(255) NOT NULL,
   `created_at` datetime(6) DEFAULT NULL,
   `accept_time` datetime(6) DEFAULT NULL,
-  `booking_id` int DEFAULT NULL,
+  `booking_id` varchar(255) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL,
-  `user_id` int DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -106,13 +109,13 @@ DROP TABLE IF EXISTS `service`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `service` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` varchar(255) NOT NULL,
   `created_at` datetime(6) DEFAULT NULL,
   `describe` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `price` double DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -121,7 +124,7 @@ CREATE TABLE `service` (
 
 LOCK TABLES `service` WRITE;
 /*!40000 ALTER TABLE `service` DISABLE KEYS */;
-INSERT INTO `service` VALUES (1,NULL,'Là chức năng cơ bản ban đầu tại Việt Nam trên ứng dụng Grab. Cho phép kết nối trực tiếp giữa khách hàng có nhu cầu đi lại và tài xế xe ôm 2 bánh (xe máy)','GrabBike',4000),(2,NULL,'Là dịch vụ xe hơi 4 bánh kết nối giữa cá nhân có xe hơi rảnh và khách hàng muốn đi bằng xe 4 bánh','GrabCar',10000);
+INSERT INTO `service` VALUES ('54bd1fe4-c478-4056-b5b8-6b37d644d782','2022-12-25 16:55:53.645155','Là chức năng cơ bản ban đầu tại Việt Nam trên ứng dụng Grab. Cho phép kết nối trực tiếp giữa khách hàng có nhu cầu đi lại và tài xế xe ôm 2 bánh (xe máy)','GrabBike',4000),('9c0f1e12-95d8-4494-97a3-aac1d22a3900','2022-12-25 16:55:13.870099','Là dịch vụ xe hơi 4 bánh kết nối giữa cá nhân có xe hơi rảnh và khách hàng muốn đi bằng xe 4 bánh','GrabCar',10000);
 /*!40000 ALTER TABLE `service` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -133,7 +136,7 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` varchar(255) NOT NULL,
   `created_at` datetime(6) DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
@@ -142,7 +145,7 @@ CREATE TABLE `user` (
   `role` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -151,7 +154,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,NULL,'Nam Định','thanh@gmail.com','$2a$10$38mrjyCOCnqKqs..zcLRG.NYUmbsR848rCAs5p23whRlCQd.CxGc2','0947298695','ROLE_CLIENT','thanhnguyen'),(2,NULL,'Nam Định','vinh@gmail.com','$2a$10$3NpaAdSoeqj8EsHxgAFiFe2/zyCC/ZboeTnhMhW7GVj.zectXOYFe','0947298695','ROLE_DRIVER','vinhnguyen');
+INSERT INTO `user` VALUES ('c230680a-f169-4058-b118-89c5b4daf496','2022-12-25 20:49:16.825024','Nam Định','thanh@gmail.com','$2a$10$X2rNkOgqB2MAqhlexey1b.PgTIy3JIGLBQcYNjs2NGzXEaB1qdl0C','0947298695','ROLE_CLIENT','thanhnguyen'),('e0ea0e74-dccb-4343-a32b-75dadac3fb16','2022-12-25 20:49:42.744164','Nam Định','vinh@gmail.com','$2a$10$YVkaSn/Plg7pC07DVfsHHusRUsXLcatOuTQYIG./r9S4l6/1HoAeu','0947298695','ROLE_DRIVER','vinhnguyen');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -164,4 +167,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-22  0:05:34
+-- Dump completed on 2022-12-25 21:14:38
